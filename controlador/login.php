b7277ea11ac0c4685a1a93917a8c2dea2023447f
master2415 <duutabarquino2415@gmail.com>
2023-11-07 09:33:11 -0500

    first commit

---
+++ controlador/login.php
@@ -0,0 +1,8 @@
+<?php
+    if(!empty($_POST["btningresar"])){
+        if (!empty($_POST["usuario"])and !empty($_POST["contrasena"])) {
+        } else {
+            echo "Hay campos vacios"
+        }
+    }
+?>
---
+++ modelo/conexion.php
@@ -0,0 +1,5 @@
+<?php
+$conexion=new mysql("localhost", "root", "root", "expendio", "3306");
+$conexion->set_charset("utf8");
+
+?>
